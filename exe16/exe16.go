b8f24259783ccd9e8ca894f25bd34a64ea5e350a
jordan <jordan@localhost.localdomain>
2020-06-06 06:49:47 -0600

    updated exe16 with exe17's task

--- exe16/exe16.go
+++ exe16/exe16.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	//Create a program that shows the “if statement” in action.
 
-	const x = 5
+	const x = 20
 	const y = 10
 
 	if x > y {
@@ -14,5 +14,7 @@ func main() {
 		fmt.Println("looks we did alright...")
 	} else if x == y {
 		fmt.Println("we really did something wrong...")
+	} else {
+		fmt.Println("numbers are hard")
 	}
 }
