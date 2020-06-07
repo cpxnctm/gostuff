4981b5d274da0e0898d44c242ff23e21a5192ea2
Jordan Chong <qc@Jordans-MacBook-Pro.local>
2020-06-06 20:32:57 -0600

    rewriting homework assignment

--- exe15/exe15.go
+++ exe15/exe15.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 //Print out the remainder (modulus) which is found for each number between 10 and 100 when it is divided by 4.
 
 func main() {
 	for i := 10; i <= 100; i++ {
-		fmt.Printf("When %v is divided by 4, the result is %v\n", i, i%4) // don't forget that you don't need to write an entire function to get the result. here, we do the math right in the print function
+		fmt.Printf("When %v is divided by 4, the remainder is: %v\n", i, i%4)
 	}
 }
