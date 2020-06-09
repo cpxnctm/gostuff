7bb31b53e01aaf80e12522234bb6a0fbce4157a6
jordan <jordan@localhost.localdomain>
2020-06-09 08:41:57 -0600

    adding exe21

--- exe20/exe20.go
+++ exe20/exe20.go
@@ -8,10 +8,31 @@ func main() {
 
 	//Using a COMPOSITE LITERAL: create a SLICE of TYPE int and assign 10 values
 
-	x := []int{5, 10, 15, 20, 25, 30, 35, 40, 45, 50}
-	for r := range x {
-		fmt.Printf("%d\n", x[r]) // range over the slice and print out the values
-		fmt.Printf("%T\n", x[r]) //range over the slice and print out the type
-	}
+	a := []int{42, 43, 44, 45, 46}
+	b := []int{47, 48, 49, 50, 51}
+	c := []int{44, 45, 46, 47, 48}
+	d := []int{43, 44, 45, 46, 47}
 
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+	//for r := range a {
+	//fmt.Printf("%v", a[r]) // range over the slice and print out the values
+	//fmt.Printf("%T\n", a[r]) //range over the slice and print out the type
+	//for i := range b {
+	//fmt.Printf("%v", b[i]) // range over the slice and print out the values
+	//fmt.Printf("%T\n", b[i])
+	//for v := range c {
+	//fmt.Printf("%v", c[v]) // range over the slice and print out the values
+	//fmt.Printf("%T\n", c[v])
+	//for n := range d {
+	//fmt.Printf("%v", d[n]) // range over the slice and print out the values
+	//	fmt.Printf("%T\n", d[n])
+
+	//}
+	//}
+	//}
+	//}
 }
---
+++ exe21/exe21.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	fmt.Println(x)
+	x = append(x, 52)
+	fmt.Println(x)
+	x = append(x, 53, 54, 55)
+	fmt.Println(x)
+	y := []int{56, 57, 58, 59, 60}
+	xy := append(x, y...)
+	fmt.Println(xy)
+
+	for r := 0; r < 18; r++ {
+		fmt.Printf("%T\n", r)
+
+	}
+}
