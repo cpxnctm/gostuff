7f2c640df0708b76324b0338007a531bfea04473
Jordan Chong <qc@Jordans-MacBook-Pro.local>
2020-06-15 17:29:58 -0600

    adding function exe 01

---
+++ exe31/exe31.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+//create a function named foo that returns an int
+//create a function named bar that returns an int and a string
+// call both functions and print their results
+func main() {
+	a := foo(2)
+	fmt.Println(a)
+	b := bar()
+	fmt.Println(b, c)
+}
+func foo(x int) int {
+	for i := 0; i < 10; i++ {
+		return i
+	}
+}
+func bar(int, string) {
+	b := "i'm writing code"
+	c := 451
+
+}
