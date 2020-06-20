d61d348782c7857a5cbaa2b60563a4b309fb74f6
Jordan Chong <qc@Jordans-MacBook-Pro.local>
2020-06-19 18:27:19 -0600

    updated hw

--- exe39/exe39.go
+++ exe39/exe39.go
@@ -13,8 +13,13 @@ func main() {
 	fmt.Println(x)
 	foo(&x)
 	fmt.Println(x)
+	bar(&x)
+	fmt.Println(x)
 }
 func foo(p *person) {
 	p.name = "Bootsy Kitty"
 
+}
+func bar(p2 *person) {
+	p2.name = "Buff Kitty"
 }
