4840dbe403f0b5716caf106e2dfb8b031b51b823
jordan <jordan@localhost.localdomain>
2020-06-23 10:16:01 -0600

    added a cost calculation return to bcrypt

--- bcrypt01/bcrypt01.go
+++ bcrypt01/bcrypt01.go
@@ -14,12 +14,17 @@ func main() {
 		fmt.Println("Error: ", err)
 	}
 	fmt.Println(spwd)
-
 	err = bcrypt.CompareHashAndPassword(spwd, []byte(pwd))
 	if err != nil {
 		fmt.Println("The passwords do not match. Please try again")
 	} else {
 		fmt.Println("Authentication Successful")
+	}
+	cst, oops := bcrypt.Cost([]byte(spwd))
+	if oops != nil {
+		fmt.Println("Error: ", oops)
+	} else {
+		fmt.Println(cst)
 	}
 
 }
