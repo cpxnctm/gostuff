5eb2d657681564e5e3d38e821352cac318fc297f
Jordan Chong <qc@Jordans-MacBook-Pro.local>
2020-06-26 17:04:41 -0600

    fixed the nonsense with exe43

--- exe43/exe43.go
+++ exe43/exe43.go
@@ -10,23 +10,22 @@ var mu sync.Mutex
 
 func main() {
 
-	lim := 100
+	lim := 50
 	counter := 0
 	wg.Add(lim)
 
 	go func() {
 		for i := 0; i < lim; i++ {
 			mu.Lock()
-			i = counter
+			i := counter
 			i++
 			counter = i
 			fmt.Println(counter)
 			mu.Unlock()
 			wg.Done()
 		}
-		fmt.Println("The current value is:", counter)
 	}()
-
 	wg.Wait()
 	fmt.Println("Program Exiting")
+
 }
