23eee2d04a2232f9c64006a1e75da24bfc4e44af
jordan <jordan@localhost.localdomain>
2020-06-26 08:38:52 -0600

    updated documentation on method set and type implementations

--- exe41/exe41.go
+++ exe41/exe41.go
@@ -3,8 +3,15 @@ package main
 import "fmt"
 
 // “The method set of a type determines the INTERFACES that the type implements.....” ~ golang spec
-//The method 'walk' which takes in the type 'person' is called in the interface 'speak'
+//The method 'saysomething' which takes in the type 'person' is called in the interface 'speak'
 // even though the speak method takes in a pointer of person, in func.main, you can still pass in a non-pointer value by calling person.speak
+//
+//This explanation didn't seem as obvious so I dug a bit more and found this answer on stackoverflow which makes more sense (at first sight). what do you thing?
+//https://stackoverflow.com/questions/42477951/what-is-the-method-set-of-sync-waitgroup
+//This is in Spec: Calls:
+
+//If x is addressable and &x's method set contains m, x.m() is shorthand for (&x).m().
+//So when you have a value that is addressable (a variable is addressable), you may call any methods that have pointer receiver on non-pointer values, and the compiler will automatically take the address and use that as the receiver value.
 
 type person struct {
 	name  string
@@ -17,7 +24,7 @@ type human interface {
 }
 
 func (p *person) speak() {
-	fmt.Println("I am:", p)
+	fmt.Println("I am:", p.name, "I am:", p.age, "I like:", p.hobby)
 }
 
 func saySomething(h human) {
