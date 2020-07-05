3b8ac1d761aeb3340c245d2637578403466b3aa9
jordan <jordan@localhost.localdomain>
2020-07-04 19:33:02 -0600

    fixed exe49

--- exe49/exe49.go
+++ exe49/exe49.go
@@ -7,7 +7,6 @@ import (
 func main() {
 	q := make(chan int)
 	c := gen(q)
-
 	receive(c)
 
 	fmt.Println("about to exit")
@@ -15,18 +14,18 @@ func main() {
 
 //use a for loop statement to pull the information off of the channel
 
-func gen(q <-chan int) <-chan int {
-	c := make(chan int)
-
-	for i := 0; i < 100; i++ {
-		c <- i
-	}
-	close(c)
-	return c
-}
-
 func receive(n <-chan int) {
 	for i := range n {
 		fmt.Println(i)
 	}
+}
+func gen(q chan int) <-chan int {
+	c := make(chan int)
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+	return c
 }
