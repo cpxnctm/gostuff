40f29d83600487073284be5107af132abf697374
Jordan Chong <qc@Jordans-MacBook-Pro.local>
2020-07-10 15:36:40 -0600

    updated the documentation to reflect the error return

--- go_doc_exes/doc01/dog/main.go
+++ go_doc_exes/doc01/dog/main.go
@@ -1,7 +1,8 @@
 //Package dog converts human years to dog years. Dogs are reportedly 7 times the age of a human
 package dog
 
-//Years will take an integer and multiply it by 7 which converts human to dog years
+//Years will take an integer and multiply it by 7 which converts human to dog years.
+//If the provided integer is negative, then Years will return an error string
 func Years(n int) (int, string) {
 	var a int
 	var b string
