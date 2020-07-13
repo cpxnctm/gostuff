c79e02e0a64da729fab282447fec027d51f96b4b
Jordan Chong <qc@Jordans-MacBook-Pro.local>
2020-07-13 16:44:54 -0600

    updated the test to a table test

--- go_testing/test01/main_test.go
+++ go_testing/test01/main_test.go
@@ -3,16 +3,21 @@ package main
 import "testing"
 
 func TestMult(t *testing.T) {
-	x := mult(2, 3, 4)
-	y := mult(5, 6, 7)
-	z := mult(8, 9, 10)
-	if x != 240 {
-		t.Errorf("Expected 240, got %v", x)
+
+	type test struct {
+		data   []int
+		answer int
 	}
-	if y != 2100 {
-		t.Errorf("Expected 2100, got %v", y)
+	tst := []test{
+		test{[]int{2, 3, 4}, 240},
+		test{[]int{5, 6, 7}, 2100},
+		test{[]int{8, 9, 10}, 7200},
 	}
-	if z != 7200 {
-		t.Errorf("Expected 7200, got %v", z)
+	for _, v := range tst {
+		x := mult(v.data...)
+		if x != v.answer {
+			t.Error("Expected:", v.answer, "Got:", x)
+		}
+
 	}
 }
