06fba2034210491c9704a9f2c24d785581daf91f
jordan <jordan@localhost.localdomain>
2020-07-16 07:29:54 -0600

    BET exercise 2

--- go_testing/testing04/word/main.go
+++ go_testing/testing04/word/main.go
@@ -1,6 +1,8 @@
 package word
 
-import "strings"
+import (
+	"strings"
+)
 
 // no need to write an example for this one
 // writing a test for this one is a bonus challenge; harder
@@ -13,6 +15,16 @@ func UseCount(s string) map[string]int {
 	return m
 }
 
+// Count takes in a string and returns the number of words separated by whitespace
 func Count(s string) int {
 	// write the code for this func
+	st := strings.Fields(s)
+	ct := 0
+	//var m2 []int
+
+	for i := range st {
+		ct = i
+		//m2[i]++
+	}
+	return ct
 }
